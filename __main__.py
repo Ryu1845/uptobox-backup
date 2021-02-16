1950ead6812737201c24a710cfa9b349808a1ae1
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2021-02-16 19:00:53 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,16 +1,16 @@
 import json
 import requests
 
-token1 = ""
-token2 = ""
+token1 = "" #uptobox token of the old account
+token2 = "" #uptobox token of the new account
 
-r = requests.get('https://uptobox.com/api/user/files?token=' + token + '&orderBy=file_name&dir=asc&offset=0&path=%2F%2F&limit=100')
+r = requests.get('https://uptobox.com/api/user/files?token=' + token1 + '&orderBy=file_name&dir=asc&offset=0&path=%2F%2F&limit=100')
 loaded_json = json.loads(r.text)
 page_counter = loaded_json["data"]["pageCount"]
 
 for i in range(page_counter):
     try:
-        r2 = requests.get('https://uptobox.com/api/user/files?token=' + token + '&orderBy=file_name&dir=asc&offset=' + str(i * 100) + '&path=%2F%2F&limit=100')
+        r2 = requests.get('https://uptobox.com/api/user/files?token=' + token1 + '&orderBy=file_name&dir=asc&offset=' + str(i * 100) + '&path=%2F%2F&limit=100')
         loaded_json2 = json.loads(r2.text)
         for x in loaded_json2["data"]["files"]:
             print(x["file_name"] + " (https://uptobox.com/" + x["file_code"] + "/)")
