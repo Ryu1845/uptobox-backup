8074f2eef9807a6a3054ecec43655ca46f8c7df8
Ryu18 <ryu@tpgjbo.xyz>
2021-05-08 19:20:04 +0200

    rewritten + added recursivity

--- __main__.py
+++ __main__.py
@@ -1,19 +1,82 @@
-import json
+import argparse
+
 import requests
 
-token1 = "" #uptobox token of the old account
-token2 = "" #uptobox token of the new account
+parser = argparse.ArgumentParser(
+    prog="uptobox_backup",
+    description="Backup the content of an uptobox account to another",
+)
+parser.add_argument(
+    "token1",
+    metavar="token1",
+    type=str,
+    help="Token for the first account",
+)
+parser.add_argument(
+    "token2",
+    metavar="token2",
+    type=str,
+    help="Token for the second account",
+)
+args = parser.parse_args()
 
-r = requests.get('https://uptobox.com/api/user/files?token=' + token1 + '&orderBy=file_name&dir=asc&offset=0&path=%2F%2F&limit=100')
-loaded_json = json.loads(r.text)
-page_counter = loaded_json["data"]["pageCount"]
+token1 = args.token1  # uptobox token of the old account
+token2 = args.token2  # uptobox token of the new account
 
-for i in range(page_counter):
+
+def alias_path(path):
+    print(f"{path=}")
+    params_init = {
+        "token": token1,
+        "orderBy": "file_name",
+        "dir": "asc",
+        "offset": 0,
+        "path": path,
+        "limit": 100,
+    }
+    init_res = requests.get(
+        "https://uptobox.com/api/user/files",
+        params=params_init,
+    )
+    loaded_json = init_res.json()
     try:
-        r2 = requests.get('https://uptobox.com/api/user/files?token=' + token1 + '&orderBy=file_name&dir=asc&offset=' + str(i * 100) + '&path=%2F%2F&limit=100')
-        loaded_json2 = json.loads(r2.text)
-        for x in loaded_json2["data"]["files"]:
-            print(x["file_name"] + " (https://uptobox.com/" + x["file_code"] + "/)")
-            r3 = requests.get('https://uptobox.com/api/user/file/alias?token=' + token2 + '&file_code=' + x["file_code"])
-    except:
-        console = False
+        page_counter = loaded_json["data"]["pageCount"]
+        folders = [folder["fld_name"] for folder in loaded_json["data"]["folders"]]
+    except TypeError:
+        print(init_res.url)
+        raise RuntimeError(
+            f"Code {loaded_json['statusCode']}: {loaded_json['message']} 1"
+        )
+
+    for page in range(page_counter):
+        param_file_list = {
+            "token": token1,
+            "orderBy": "file_name",
+            "dir": "asc",
+            "offset": str(page * 100),
+            "path": path,
+            "limit": 100,
+        }
+        file_list_res = requests.get(
+            "https://uptobox.com/api/user/files", params=param_file_list
+        )
+        loaded_file_list = file_list_res.json()
+        for file in loaded_file_list["data"]["files"]:
+            print(f"{file['file_name']} (https://uptobox.com/{file['file_code']}/)")
+            params_alias = {"token": token2, "file_code": file["file_code"]}
+            res = requests.get(
+                "https://uptobox.com/api/user/file/alias", params=params_alias
+            )
+            data = res.json()
+            if data["statusCode"] != 0:
+                raise RuntimeError(f"Code {data['statusCode']}: {data['message']} 2")
+    for folder in folders:
+        alias_path(folder)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    alias_path("//")
